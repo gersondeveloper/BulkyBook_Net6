14fea07a63ad1f58fa03fd333fb27bdc04bfa992
Gerson Cardoso Filho <gersoncflho@mac.com>
2022-09-20 09:07:46 +0100

    Initial commit

---
+++ src/BulkiBook_Net6_DataAccess/Context/MyDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BulkiBook_Net6_DataAccess.Entities;
+
+namespace BulkiBook_Net6_DataAccess.Context
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=BulkyBook_New;User Id=sa;Password=MyPass@word;Trusted_Connection=False; ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ src/BulkiBook_Net6_DataAccess/Entities/Category.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkiBook_Net6_Web.Models;
+
+[Table("Categories")]
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
