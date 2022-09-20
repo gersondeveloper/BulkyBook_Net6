3a7c6e740ead28f16c79fa37ced23cfdee38bd90
Gerson Cardoso Filho <gersoncflho@mac.com>
2022-09-20 09:10:11 +0100

    changed namespace

--- src/BulkiBook_Net6_DataAccess/Entities/Category.cs
+++ src/BulkiBook_Net6_DataAccess/Entities/Category.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BulkiBook_Net6_Web.Models;
+namespace BulkiBook_Net6_DataAccess.Entities;
 
 [Table("Categories")]
 public class Category
